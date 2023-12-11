f9ee403bf9c9c9fd60377a95e6a110c838943af4
AniseRe777 <146992510+AniseRe777@users.noreply.github.com>
2023-12-11 13:33:36 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Scroll to top button
+        const scrollToTopButton = document.getElementById('scrollToTopBtn');
+        function scrollToTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
+        scrollToTopButton.addEventListener('click', scrollToTop);
+        window.addEventListener('scroll', function () {
+            if (document.documentElement.scrollTop > 100) {
+                scrollToTopButton.style.display = 'block';
+            } else {
+                scrollToTopButton.style.display = 'none';
+            }
+        });
