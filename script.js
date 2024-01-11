c439ed31999bc89b3be07a43dd383e2a807efa1e
AniseRe777 <146992510+AniseRe777@users.noreply.github.com>
2024-01-11 14:30:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,31 @@
             }
         });
 
+// Event overlay size
+document.addEventListener("DOMContentLoaded", function () {
+    // Wait for the DOM to be fully loaded
+
+    // Get all elements with the class 'event-overlay'
+    var overlays = document.querySelectorAll('.event-overlay');
+
+    // Initialize max height
+    var maxHeight = 0;
+
+    // Loop through each overlay to find the max height
+    overlays.forEach(function (overlay) {
+        var overlayHeight = overlay.offsetHeight;
+        if (overlayHeight > maxHeight) {
+            maxHeight = overlayHeight;
+        }
+    });
+
+    // Set the same height for all overlays
+    overlays.forEach(function (overlay) {
+        overlay.style.height = maxHeight + 'px';
+    });
+});
+
+
 // For switching between sections
 function showSection(sectionId) {
     // Hide all sections
